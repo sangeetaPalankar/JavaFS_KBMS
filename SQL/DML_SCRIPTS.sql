964f237832d31cb69590bf879ea58057cc3a6185
Sangeeta Palankar <89837405+sangeetaPalankar@users.noreply.github.com>
2023-01-18 17:16:14 +0530

    SQL

--- SQL/DML_SCRIPTS.sql
+++ SQL/DML_SCRIPTS.sql
@@ -1,33 +1,57 @@
-/*  
-	Author Sangeeta Palankar
-    date 28/12/2022
-    DML
-*/
-#INSERT COMMANDS
 USE KBMS_PROJECT;
-INSERT INTO USER
-(FNAME, LNAME, EMAIL, PHONE_NO, ADDRESS, DATE_OF_BIRTH, ID_PROOF, LOGIN_ID, PASSWORD)
-VALUES
-("Sangeeta", "Palankar", "sangita123@gmail.com", '9845655245',"HNO. 47 Belgaum", "2000-10-18", '458769523541',"sangita123", "qwfsrt56"),
-("Mrunal", "Barade", "mrunal23@gmail.com", "9845855245","HNO. 42 Pune", "2000-12-12", "458775623541","__mrunal_", "qowusrt56"),
-("Eshwari", "Burlawar", "eshari01@gmail.com", "4585655245","HNO. 37 Mumbai", "2001-10-18", "458762623541","eshwari04", "iofsrt56"),
-("Rahul", "Joshi", "rahul23@gmail.com", "4545655245","HNO. 23 Bangalore", "2000-02-28", "569769523541","_rahuk09", "q5fsrt56"),
-("Aanchal", "Joshi", "aanchal1@gmail.com", "9845755245","HNO. 67 Delhi", "2001-10-14", "458756523541","anchal32_", "kwfsrt56");
-
 INSERT INTO PRODUCT 
-(PRODUCT_ID, PRODUCT_NAME, PRODUCT_COUNT, TOTAL_STOCK, BEST_BEFORE, PRICE_PER_UNIT)
+(PRODUCT_ID, PRODUCT_NAME, PRODUCT_COUNT, TOTAL_STOCK, BEST_BEFORE, PRICE_PER_UNIT,TYPE_OF_PRODUCT)
 VALUES
-(10001, "Apple", 200, 500, '2023-03-23', 150),
-(10002, "Tomato", 80, 600, '2023-03-30', 50),
-(10003, "Coffe", 40, 800, '2023-05-23', 250),
-(10004, "Turmeric", 50, 30, '2023-03-23', 150);
+(101, "Apple", 200, 500, '2023-03-23', 150,"fruit"),
+(102, "orange", 80, 600, '2023-03-30', 50,"fruit"),
+(103, "Mango", 40, 800, '2023-05-23', 250,"fruit"),
+(104, "Grapes", 50, 30, '2023-03-23', 150,"fruit"),
+(105, "Potato", 200, 500, '2023-03-23', 150,"vegitable"),
+(106, "Carrot", 80, 600, '2023-03-30', 50,"vegitable"),
+(107, "Lady Finger", 40, 800, '2023-05-23', 250,"vegitable"),
+(108, "onion", 50, 30, '2023-03-23', 150,"vegitable"),
+(109, "Nutmeg", 200, 500, '2023-03-23', 150,"Spices"),
+(110, "Black Pepper", 80, 600, '2023-03-30', 50,"Spices"),
+(111, "Cardamom", 40, 800, '2023-05-23', 250,"Spices"),
+(112, "Saffron", 50, 30, '2023-03-23', 150,"Spices");
+
+INSERT INTO Customer
+(
+`Customer_name`, 
+`email` , 
+`phone` ,
+`address` , 
+`date_of_birth`, 
+`age` ,
+`password`,
+`identity_proof`,
+`preferred_login_id`)
+VALUES
+("Alexa","al9090@hmail.com", 9090787856, "Street 12, green tower, Jordan", 
+    "1989-10-10", 32, "xxxxxxx", "visa card", "al9090@hmail.com"),
+    
+    ("Alexander","polo90@hmail.com", 9898990908, "Street 3, twin tower, tordan", 
+    "1978-02-19", 43, "xxxxxxx", "visa card", "polo90@hmail.com"),
+    
+    ("Marie","al9090@hmail.com", 9898989907, "Street 3, twin tower, Jordan", 
+    "2001-10-28", 21, "xxxxxxx", "visa card", "al9090@hlmail.com"),
+    
+    ("Jacky","loki0@hmail.com", 9897856489, "Street 2,quad tower, london", 
+    "1978-09-23", 44, "xxxxxxx", "visa card", "loki@hmail.com"),
+    
+    ("Bob","bob@hmail.com", 9898907869, "Street 3, twin tower, Jordan", 
+    "2002-09-11",20, "xxxxxxx", "visa card", "bob@hmail.com"),
+    
+    ("Alexa","al9090@hmail.com", 9894523889, "Street 3, twin tower, Jordan", 
+    "1999-11-10", 23, "xxxxxxx", "visa card", "alexa_9090"),
+    
+    ("Sonail","sonail@hmail.com", 9897890589, "Street 3, twin tower, Jordan", 
+    "1993-12-12", 29, "xxxxxxx", "visa card", "sonail_675");
+
 
 INSERT INTO ORDER_DETAILS
-(ORDER_ID, PRODUCT_NAME, QUANTITY, TOTAL_PRICE, PRODUCT_ID, LOGIN_ID)
+(ORDER_ID, preferred_login_id, PRODUCT_ID, QUANTITY, TOTAL_PRICE, ORDER_DATE, STATUS, CANCELLATION_DATE, CANCELLATION_REASON)
 VALUES 
-("1001","Coffee", 5 ," #trigger ", 10003, "anchal32"),
-("1002","Coffee", 5 ," #trigger ", 10003, "anchal32"),
-("1003","Coffee", 5 ," #trigger ", 10003, "_mrunal_");
-
-#DELETE COMMANDS
-DELETE FROM PRODUCT 
+(10001,"sonail_675", 101 , 1, 100.00,'2022-10-23',"Confirmed", null, null),
+(10002,"alexa_9090",106 , 2, 200.00 , '2022-10-20',"Confirmed",null,null),
+(10003,"bob@hmail.com",108,1,300.00, '2022-11-23',"Confirmed", null,null);
